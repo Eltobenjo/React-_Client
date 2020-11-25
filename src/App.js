a29f77e30f33d700e94497c1f5e1a295c985c027
Elton Benjamin <eltobenjo@gmail.com>
2020-11-25 17:49:45 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+ 
+function App() {
+  const [data, setData] = useState({ hits: [] });
+ 
+  useEffect(() => {
+   
+     fetch( 'http://localhost:4000',
+    )
+    .then((res)=>res.json())
+    .then((res)=>setData(res) );
+
+  
+  },[]);
+ 
+  return (
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
+  );
+}
+ 
+export default App;
